52d648597c988cccc639d108417ba07b600077cf
303-1 <jilee317@gmail.com>
2018-01-16 11:10:33 +0900

    2018_01_16 이재인 Basic type까지 코틀린 공부중

--- app/src/main/java/yi/jaein/musicplayer/test.kt
+++ app/src/main/java/yi/jaein/musicplayer/test.kt
@@ -43,4 +43,40 @@ fun test(){
     println(array.get(0)
     prinntln(array[0])
     println(array.size)
+
+    특별한 Array 클래스
+    -primitive 타입의 박싱 오버헤드를 없애긱 위한 배열
+    -IntArray,ShortArray,IntArray
+    -Array를 상송한 클래스들은 아니지만 Array와 같은 메소드와 프로퍼티를 가짐
+    -size등 유용한 멤버 함수 포함
+    val x: IntArray = intArrayOf(1,2,3)
+    x[0] =7
+    println(x.get(0))
+    println(x[0])
+    println(x.size)
+
+    Kotlin 문자열
+    -문자열은 String 클래스로 표현
+    -String 은 characters로 구성됨
+    -s[i]와 같은 방식으로 접근 가능
+
+    var x:String ="Kotlin
+    println(x.get(0)
+    println(x[0])
+    println(x.length)
+
+    for(c in x){
+    println(c)
+    }
+
+
+    -문자열 리터럴
+    -escaped string ("Kotln")
+    전통적인 방식으로 Java String 과 거의 비슷
+    Backslash를 사용하여  excaping 처리
+
+    -raw string(""Kotlin"")
+    escaping 처리 필요 없음
+    개행이나 어더한 문자 포함가능
+
  */
